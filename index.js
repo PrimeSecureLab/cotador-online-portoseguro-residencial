9e54922641f2504386536e2bb4f5cb6d23425135
Matheus Marques <matheus.marques.aquino@gmail.com>
2023-05-10 12:07:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ dotenv.config();
 
 // Conecta ao banco de dados
 database.mongoose
+<<<<<<< Updated upstream
 .connect( database.url,
   { useNewUrlParser: true, useUnifiedTopology: true }
 ).then( () => {
@@ -26,12 +27,20 @@ database.mongoose
 }).catch( error => {
   console.log('Não foi possivel realizar a conexão com o banco de dados!', error);
   process.exit();
+=======
+    .connect(database.url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { console.log("Conexão com o banco de dados estabelecida com sucesso!"); })
+    .catch((error) => { 
+        console.log( "Não foi possivel realizar a conexão com o banco de dados!", error );
+        process.exit();
+>>>>>>> Stashed changes
 });
 
 // Cria a aplicação Express
 const app = express();
 
 // Remove extensão .html
+<<<<<<< Updated upstream
 app.use((req, res, next)=>{
   if (req.path.endsWith('.html')) {
     let newPath = req.path.slice(0, -5);
@@ -52,6 +61,28 @@ app.use(session({
   saveUninitialized: true,
   resave: true
 }));
+=======
+app.use((req, res, next) => {
+    if (req.path.endsWith(".html")) {
+        let newPath = req.path.slice(0, -5);
+        res.redirect(301, newPath);
+    } else { next(); }
+});
+
+// Armazena sessões no MongoDB
+const store = new sessionStore({ uri: process.env.DB_URL, collection: "sessoes", });
+store.on("error", function (error) { console.log(error); });
+
+app.use(
+    session({
+        secret: process.env.DB_SECRET_TOKEN,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 }, // Um ano
+        store: store,
+        saveUninitialized: true,
+        resave: true,
+    })
+);
+>>>>>>> Stashed changes
 
 // Configura o body-parser nativo do Express
 app.use(express.json());
@@ -70,6 +101,4 @@ app.use('/obrigado', obrigadoRoutes);
 
 // Inicia o servidor
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
+app.listen(port, () => { console.log(`Servidor rodando na porta ${port}`); });
