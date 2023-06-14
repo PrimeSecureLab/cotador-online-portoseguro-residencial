94cfcb80455b82cd0433ecf7fc1dfc93433aef1e
Matheus Marques <49125487+Matheus-Marques-Aquino@users.noreply.github.com>
2023-06-14 07:57:46 -0300

    Update pagamento.js

--- routes/pagamento.js
+++ routes/pagamento.js
@@ -9,6 +9,9 @@ const Propostas = require('../collections/propostas');
 const authToken = require('../configs/authToken');
 const validation = require('../configs/validation');
 
+const ValidarCotacao = require('../configs/validarCotacao');
+var validacaoCotacao = new ValidarCotacao;
+
 dotenv.config();
 
 router.get("/", async (req, res) => {
@@ -38,70 +41,54 @@ router.post("/", async (req, res) => {
     if (!body){ return res.status(400).json({fatal: 3}); }
     if (!body.formData){ return res.status(400).json({redirect: '/'}); } //Verifica se dados da Cotação ainda são válidos
     if (!body.pagamento){ return res.status(400).json({fatal: 4}); }
+
     if (!body.produto){ return res.status(400).json({fatal: 5}); }
-
-    /*let session = (req.session) ? req.session : {};
-    if (!session.user_id){ return res.status(400).json({fatal: 1}); }
-    
-    let user = await Usuarios.findOne({_id: session.user_id});
-    if (!user){ req.session.destroy((e) => { return res.status(400).json({redirect: '/login'}); }); }
-
-    let data = req.body;
-    let errorList = [];
-
-    console.log(data);
-
-    if (!data){ return res.status(400).json({fatal: 3}); }
-    if (!data.formData){ return res.status(400).json({redirect: '/'}); }
-    if (!data.pagamento){ return res.status(400).json({fatal: 4}); }
-    if (!data.produto){ return res.status(400).json({fatal: 5}); }
-    
-
     let produto = data.produto;
-    if (produto != 'habitual' && produto != 'habitual-premium' && produto != 'veraneiro'){ return res.status(400).json({fatal: 6}); }
 
+    if (produto != 'habitual' && produto != 'habitual-premium' && produto != 'veraneiro'){ return res.status(400).json({fatal: 6}); }
     let bytes = CryptoJS.AES.decrypt(data.formData, process.env.CRYPTO_TOKEN);
     if (!bytes){ return res.status(400).json({redirect: '/'}); }
     
     let formData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
     if (!formData){ return res.status(400).json({redirect: '/'}); }
-    formData.item = data.itemData;
-    
-    data.pagamento.codigoBandeira = 0;
-    formData.segurado.email = user.email;
 
-    if (data.pagamento.nomeImpresso.length < 3){ 
+    formData.item = data.itemData;    
+
+    let pagamento = data.pagamento || {};
+    pagamento.codigoBandeira = 0;
+
+    if (pagamento.nomeImpresso.length < 3){ 
         errorList.push({message: "Nome inválido", id: "nome_impresso"}); 
     }else{
-        if (!/^[A-zÀ-ú/\s]+$/.test(data.pagamento.nomeImpresso)){ errorList.push({message: "Nome inválido"}); }
+        if (!/^[A-zÀ-ú/\s]+$/.test(pagamento.nomeImpresso)){ errorList.push({message: "Nome inválido"}); }
     }
-    if (!/^\d{2,4}$/.test(data.pagamento.numeroCvc)){ errorList.push({message: "CVC inválido", id: "cvc"}); } 
-    if (!/^\d{2}$/.test(data.pagamento.mesValidade)){ 
-        if (/^[1-9]$/.test(data.pagamento.mesValidade)){// Caso tenha apenas 1 dígito 
-            data.pagamento.mesValidade = `0${data.pagamento.mesValidade}`; 
-            $("#mes").val(data.pagamento.mesValidade); 
+    if (!/^\d{2,4}$/.test(pagamento.numeroCvc)){ errorList.push({message: "CVC inválido", id: "cvc"}); } 
+    if (!/^\d{2}$/.test(pagamento.mesValidade)){ 
+        if (/^[1-9]$/.test(pagamento.mesValidade)){// Caso tenha apenas 1 dígito 
+            pagamento.mesValidade = `0${pagamento.mesValidade}`; 
+            $("#mes").val(pagamento.mesValidade); 
         }else{ // Caso tenha mais de 2 digitos
             errorList.push({message: "Mês inválido", id: "mes"}); 
         }
     }else{
-        if (parseInt(data.pagamento.mesValidade) > 12){ errorList.push({message: "Mês inválido", id: "mes"}); }
+        if (parseInt(pagamento.mesValidade) > 12){ errorList.push({message: "Mês inválido", id: "mes"}); }
     }
-    if (!/^\d{4}$/.test(data.pagamento.anoValidade)){ 
-        if (/^\d{2}$/.test(data.pagamento.anoValidade)){
-            $("#ano").val(`20${data.pagamento.anoValidade}`);
-            if (parseInt(data.pagamento.anoValidade) < 23){ errorList.push({message: "Ano inválido", id: "ano"}); }
+    if (!/^\d{4}$/.test(pagamento.anoValidade)){ 
+        if (/^\d{2}$/.test(pagamento.anoValidade)){
+            $("#ano").val(`20${pagamento.anoValidade}`);
+            if (parseInt(pagamento.anoValidade) < 23){ errorList.push({message: "Ano inválido", id: "ano"}); }
         }else{
             errorList.push({message: "Ano inválido", id: "ano"});
         }  
     }
-    if (data.pagamento.numeroCartao){ data.pagamento.numeroCartao = data.pagamento.numeroCartao.replace(/[^0-9]+/g, ""); }
-    if (!/^\d{16}$/.test(data.pagamento.numeroCartao)){ 
+    if (pagamento.numeroCartao){ pagamento.numeroCartao = pagamento.numeroCartao.replace(/[^0-9]+/g, ""); }
+    if (!/^\d{16}$/.test(pagamento.numeroCartao)){ 
         errorList.push({message: "Número de cartão inválido", id: "numero-cartao"}); 
     }else{
         let soma = 0;
         let dobrar = false;
-        for (var i = data.pagamento.numeroCartao.length - 1; i >= 0; i--) {
-            let digito = parseInt(data.pagamento.numeroCartao.charAt(i), 10);
+        for (var i = pagamento.numeroCartao.length - 1; i >= 0; i--) {
+            let digito = parseInt(pagamento.numeroCartao.charAt(i), 10);
             if (dobrar) { if ((digito *= 2) > 9){ digito -= 9; } }
             soma += digito;
             dobrar = !dobrar;
@@ -111,10 +98,109 @@ router.post("/", async (req, res) => {
 
     if (errorList.length){ return res.status(400).json({fatal: false, errors: errorList}); }
 
-    if (!validation.eloCardPattern.test(data.pagamento.numeroCartao)){ data.pagamento.codigoBandeira = 5; }
-    if (!validation.dinersCardPattern.test(data.pagamento.numeroCartao)){ data.pagamento.codigoBandeira = 3; }
-    if (!validation.masterCardPattern.test(data.pagamento.numeroCartao)){ data.pagamento.codigoBandeira = 2; }
-    if (!validation.visaCardPattern.test(data.pagamento.numeroCartao)){ data.pagamento.codigoBandeira = 1; }
+    if (!validation.eloCardPattern.test(pagamento.numeroCartao)){ pagamento.codigoBandeira = 5; }
+    if (!validation.dinersCardPattern.test(pagamento.numeroCartao)){ pagamento.codigoBandeira = 3; }
+    if (!validation.masterCardPattern.test(pagamento.numeroCartao)){ pagamento.codigoBandeira = 2; }
+    if (!validation.visaCardPattern.test(pagamento.numeroCartao)){ pagamento.codigoBandeira = 1; }
+
+    let segurado = formData.segurado || {};
+    let endereco = segurado.endereco || {};
+    let pessoaFisica = user.pessoaFisica || {};    
+    
+    endereco.cep = endereco.cep || '';
+    endereco.cep = endereco.cep.replace(/\D/g, '');
+
+    segurado.numeroTelefone = segurado.numeroTelefone || '';
+    segurado.numeroTelefone = segurado.numeroTelefone.replace(/\D/g, '');
+
+    documento.numero  = documento.numero || '';
+    documento.numero = documento.numero.replace(/\D/g, '');
+
+    let dataNascimeto = pessoaFisica.dataNascimento || '';
+    dataNascimeto = dataNascimeto.formatarDataAmericana(dataNascimento);
+
+    let dataExpedicao = documento.dataExpedicao || '';
+    dataExpedicao = dataExpedicao.formatarDataAmericana(dataExpedicao);
+
+    pagamento.numeroParcelas = 1;
+    pagamento.formaPagamento = "CARTAO_DE_CREDITO_62";
+
+    let proposta = {        
+        "numeroOrcamento": data.orcamento.numeroOrcamento,
+        "codigoPessoaPoliticamenteExposta": pessoaFisica.pessoaPoliticamenteExposta,
+        "segurado": {
+            "enderecoCobranca": {
+                "cep": endereco.cep,
+                "tipoLogradouro": endereco.tipo,
+                "logradouro": endereco.logradouro,
+                "numero": endereco.numero,
+                "bairro": endereco.bairro,
+                "cidade": endereco.cidade,
+                "uf": endereco.uf
+            },
+            "enderecoCorrespondencia": {
+                "cep": endereco.cep,
+                "tipoLogradouro": endereco.tipo,
+                "logradouro": endereco.logradouro,
+                "numero": endereco.numero,
+                "bairro": endereco.bairro,
+                "cidade": endereco.cidade,
+                "uf": endereco.uf
+            },
+            //"contato": {
+            //   "email": segurado.email
+            //},
+            //"pessoaPoliticamenteExposta": {
+            //    "cpf": "64010333049",
+            //    "nome": "Gilbor Baohon Koxyorim",
+            //    "codigoGrauRelacionamento": 1
+            //},
+            "pessoaFisica": {
+                "dataNascimento": dataNascimeto,
+                "codigoSexo": pessoaFisica.sexo,
+                "codigoEstadoCivil": pessoaFisica.estadoCivil,
+                "codigoPaisResidencia": 237,
+                "codigoFaixaRenda": pessoaFisica.faixaRenda,
+                "documentoIdentificacao": {
+                    "tipoDocumento": documento.tipo,
+                    "numeroDocumento": documento.numero,
+                    "orgaoExpedidor": documento.orgaoExpedidor,
+                    "dataExpedicao": dataExpedicao
+                }
+            }
+        },
+        "pagamento": {
+            "formaPagamento": pagamento.formaPagamento,
+            "quantidadeParcelas": pagamento.numeroParcelas,
+            "cartao": {
+                "numeroCartao": "5408094831763990",
+                "codigoBandeira": pagamento.codigoBandeira,
+                "mesValidade": pagamento.mesValidade,
+                "anoValidade": pagamento.anoValidade
+            }
+        }
+    };
+
+    let contato = { email: user.email || '' };
+    if (numeroTelefone.length == 10){ contato.numeroTelefoneResidencial = numeroTelefone; }
+    if (numeroTelefone.length == 11){ contato.numeroTelefoneCelular = numeroTelefone; }    
+
+    let politicamenteExposta = pessoaFisica.politicamenteExposta || {};
+    politicamenteExposta.cpf = politicamenteExposta.cpf || '';
+    politicamenteExposta.cpf = politicamenteExposta.cpf.replace(/\D/g, '');
+    politicamenteExposta.nome = politicamenteExposta.nome || '';
+    politicamenteExposta.grauRelacionamento = politicamenteExposta.grauRelacionamento;
+
+    proposta.segurado.contato = contato;
+    proposta.segurado.pessoaPoliticamenteExposta = politicamenteExposta;
+    
+    /*
+    let politicamenteExposta = {
+        "cpf": pessoaFisica.politicamenteExposta.cpf,
+        "nome": pessoaFisica.politicamenteExposta.nome,
+        "codigoGrauRelacionamento": pessoaFisica.politicamenteExposta.grauRelacionamento
+    };*/
+    /*
 
     if (formData.segurado.numeroTelefone){ formData.segurado.numeroTelefone = formData.segurado.numeroTelefone.replace(/\D/g, ''); }
 
@@ -240,7 +326,7 @@ router.post("/", async (req, res) => {
     console.log('C:', proposta);
 
     console.log('pessoaFisica:', proposta.segurado.pessoaFisica);
-    */
+   
     let _proposta = {        
         "numeroOrcamento": 1686676073829,
         "codigoPessoaPoliticamenteExposta": 1,
@@ -296,7 +382,7 @@ router.post("/", async (req, res) => {
             }
         }
     };
-
+ */
     let token = await authToken();
     let header = { headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` } };
     let url = 'https://portoapi-hml.portoseguro.com.br/re/residencial/v1/habitual/propostas';
