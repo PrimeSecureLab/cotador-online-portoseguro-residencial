c24bb38da412a4ed4225bc94ac1a68141d6f3d23
Matheus Marques <49125487+Matheus-Marques-Aquino@users.noreply.github.com>
2023-06-14 07:12:02 -0300

    Update cotacao.js

--- routes/cotacao.js
+++ routes/cotacao.js
@@ -100,85 +100,79 @@ router.post("/enviar-dados", async (req, res) => {
     };
     console.log(data);
 
-    var arrayErros = [];
+    var errorList = [];
 
-    try {
-        arrayErros = [];
-        //Etapa 1:
-        if (!validation.cpfPattern.test(body.cpf)){ 
-            arrayErros.push({error: "CPF inválido.", field: "cpf", step: "1"}); 
-        }
-        if (!validation._nomePattern.test(body.nome)) { 
-            arrayErros.push({error: "Nome inválido.", field: "nome", step: "1"}); 
-        }
-        if (body.tipotelefone != 1 && body.tipotelefone != 2 && body.tipotelefone != 3) { 
-            arrayErros.push({error: "Tipo de telefone inválido.", field: "tipotelefone", step: "1"}); //0: Celular, 1: Fixo
-        }
-        if (body.numerotelefone.length != 10 && body.numerotelefone.length != 11) { 
-            arrayErros.push({error: "Número de telefone inválido.", field: "numerotelefone", step: "1"}); 
-        }else{
-            if ((body.tipotelefone == 1 || body.tipotelefone == 2) && body.numerotelefone.length != 10){
-                arrayErros.push({error: "Telefone fixo deve ter 10 digitos.", field: "numerotelefone", step: "1"});
-            }
-            if (body.tipotelefone == 3 && body.numerotelefone.length != 11){
-                arrayErros.push({error: "Telefone celular deve ter 11 digitos.", field: "numerotelefone", step: "1"});
-            }
-        }
-        if (body.datanascimento){
-            if (!/^(\d{4})\-(\d{2})\-(\d{2})$/.test(body.datanascimento)) { 
-                arrayErros.push({error: "Data de nascimento inválida.", field: "datanascimento", step: "1"}); 
-            }
-        }else{
-            arrayErros.push({error: "Data de nascimento inválida.", field: "datanascimento", step: "1"});
-        }
-        
-        //Etapa 2:
-        if (body.cep.length != 8) { 
-            arrayErros.push({error: "CEP inválido.", field: "cep", step: "2"}); 
-        }
-        if (!validation.tipoResidenciaPattern.test(body.tiporesidencia)){
-            arrayErros.push({error: "Tipo de residencia inválido.", field: "tiporesidencia", step: "2"});
-        }
-        if (!validation._enderecoPattern.test(body.logradouro)) { 
-            arrayErros.push({error: "Endereço inválido.", field: "logradouro", step: "2"}); 
-        }
-        if (!validation.listaTipoRua.includes(body.tiporua)) { 
-            arrayErros.push({error: "Tipo de rua inválido.", field: "tiporua", step: "2"}); //0: Rua, 1: Avenida
-        }
-        if (!validation.numeroPattern.test(body.numero)) { 
-            arrayErros.push({error: "Número inválido.", field: "numero", step: "2"}); 
-        }
-        if (!validation._bairroPattern.test(body.bairro)) { 
-            arrayErros.push({error: "Bairro inválido.", field: "bairro", step: "2"}); 
-        }
-        if (!validation._cidadePattern.test(body.cidade)) { 
-            arrayErros.push({error: "Cidade inválida.", field: "cidade", step: "2"}); 
-        }
-        if (!validation.ufPattern.test(body.uf.toUpperCase())) { 
-            arrayErros.push({error: "UF inválido.", field: "uf", step: "2"}); 
-        }else{
-            if (!validation.listaUF.includes(body.uf.toUpperCase())){ 
-                arrayErros.push({error: "UF inválido.", field: "uf", step: "2"}); 
-            }
-        }
-        
-        //Retorna campos com erro ao enviar formulário
-        if (arrayErros.length > 0){ return res.status(400).send(JSON.stringify(arrayErros)); }
-
-        // Adiciona informações ao banco de dados
-        var lead = data.segurado;
-        lead.dataCadastro = new Date;
-        lead = new Leads(lead);
-        try { lead = await lead.save(); data.id = lead.id; } catch (err) { console.log(err); }
-        
-        // Encripta dados a serem enviados
-        var encryptedForm = CryptoJS.AES.encrypt( JSON.stringify(data), process.env.CRYPTO_TOKEN ).toString();
-
-        res.json({ formData: encryptedForm, itemData: items });    
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Ocorreu um erro ao enviar os dados para a Porto Seguro.");
+    if (!validation.cpfPattern.test(body.cpf)){ 
+        errorList.push({error: "CPF inválido.", field: "cpf", step: "1"}); 
     }
+    if (!validation._nomePattern.test(body.nome)) { 
+        errorList.push({error: "Nome inválido.", field: "nome", step: "1"}); 
+    }
+    if (body.tipotelefone != 1 && body.tipotelefone != 2 && body.tipotelefone != 3) { 
+        errorList.push({error: "Tipo de telefone inválido.", field: "tipotelefone", step: "1"}); //0: Celular, 1: Fixo
+    }
+    if (body.numerotelefone.length != 10 && body.numerotelefone.length != 11) { 
+        errorList.push({error: "Número de telefone inválido.", field: "numerotelefone", step: "1"}); 
+    }else{
+        if ((body.tipotelefone == 1 || body.tipotelefone == 2) && body.numerotelefone.length != 10){
+            errorList.push({error: "Telefone fixo deve ter 10 digitos.", field: "numerotelefone", step: "1"});
+        }
+        if (body.tipotelefone == 3 && body.numerotelefone.length != 11){
+            errorList.push({error: "Telefone celular deve ter 11 digitos.", field: "numerotelefone", step: "1"});
+        }
+    }
+    if (body.datanascimento){
+        if (!/^(\d{4})\-(\d{2})\-(\d{2})$/.test(body.datanascimento)) { 
+            errorList.push({error: "Data de nascimento inválida.", field: "datanascimento", step: "1"}); 
+        }
+    }else{
+        errorList.push({error: "Data de nascimento inválida.", field: "datanascimento", step: "1"});
+    }
+    
+    //Etapa 2:
+    if (body.cep.length != 8) { 
+        errorList.push({error: "CEP inválido.", field: "cep", step: "2"}); 
+    }
+    if (!validation.tipoResidenciaPattern.test(body.tiporesidencia)){
+        errorList.push({error: "Tipo de residencia inválido.", field: "tiporesidencia", step: "2"});
+    }
+    if (!validation._enderecoPattern.test(body.logradouro)) { 
+        errorList.push({error: "Endereço inválido.", field: "logradouro", step: "2"}); 
+    }
+    if (!validation.listaTipoRua.includes(body.tiporua)) { 
+        errorList.push({error: "Tipo de rua inválido.", field: "tiporua", step: "2"}); //0: Rua, 1: Avenida
+    }
+    if (!validation.numeroPattern.test(body.numero)) { 
+        errorList.push({error: "Número inválido.", field: "numero", step: "2"}); 
+    }
+    if (!validation._bairroPattern.test(body.bairro)) { 
+        errorList.push({error: "Bairro inválido.", field: "bairro", step: "2"}); 
+    }
+    if (!validation._cidadePattern.test(body.cidade)) { 
+        errorList.push({error: "Cidade inválida.", field: "cidade", step: "2"}); 
+    }
+    if (!validation.ufPattern.test(body.uf.toUpperCase())) { 
+        errorList.push({error: "UF inválido.", field: "uf", step: "2"}); 
+    }else{
+        if (!validation.listaUF.includes(body.uf.toUpperCase())){ 
+            errorList.push({error: "UF inválido.", field: "uf", step: "2"}); 
+        }
+    }
+    
+    //Retorna campos com erro ao enviar formulário
+    if (errorList.length > 0){ return res.status(400).send(JSON.stringify(arrayErros)); }
+
+    // Adiciona informações ao banco de dados
+    //var lead = data.segurado;
+    //lead.dataCadastro = new Date;
+    //lead = new Leads(lead);
+    //try { lead = await lead.save(); data.id = lead.id; } catch (err) { console.log(err); }
+    
+    // Encripta dados a serem enviados
+    var encryptedForm = CryptoJS.AES.encrypt( JSON.stringify(data), process.env.CRYPTO_TOKEN ).toString();
+
+    res.json({ formData: encryptedForm, itemData: items });    
+   
 });
 
 module.exports = router;
